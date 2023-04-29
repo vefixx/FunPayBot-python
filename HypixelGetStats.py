c6b30cad386fe35975208b72e93998bc3b024171
vefixx <131001961+vefixx@users.noreply.github.com>
2023-04-29 11:30:01 +0500

    Update HypixelGetStats.py

--- HypixelGetStats.py
+++ HypixelGetStats.py
@@ -3,7 +3,7 @@ import json
 import time
 
 while True:    
-    r = requests.get('TOKEN').json()
+    r = requests.get('https://api.hypixel.net/skyblock/profile?key=(TOKEN)').json()
     coin_purse = round(r['profile']['members']['f14ac11e474d4bc3bcc8a10dd4a5e2a0']['coin_purse'], 0)
     rounded_number = int(coin_purse)
     first_two_digits = int(str(coin_purse)[:2])
